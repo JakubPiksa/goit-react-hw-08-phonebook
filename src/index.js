bf7e56a9f0a085d2c533c1a86848b11c212cb13a
Jakub Piksa <kuba.piksa@wp.pl>
2023-08-06 22:51:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
@@ -9,10 +9,15 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename="goit-react-hw-08-movies">
+      <BrowserRouter >
         <App />
-      </Router>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+//basename="goit-react-hw-08-movies"
+
